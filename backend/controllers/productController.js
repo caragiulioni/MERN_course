2360eda18248088ca949a0354f394f3c9f03e58b
Cara Giulioni <caragiulioni@gmail.com>
2021-05-11 16:02:38 -0400

    add get single product by id

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,6 +10,26 @@ exports.newProduct = async (req, res, next) => {
   });
 };
 
-exports.getProducts = (req, res, next) => {
-  res.status(200).json({ success: true, message: "Show all products" });
+//get all products => api/v1/products
+exports.getProducts = async (req, res, next) => {
+  const products = await Product.find();
+
+  res.status(200).json({ success: true, count: products.length, products });
+};
+
+//get single product details => /api/v1/product/:id
+
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
 };
