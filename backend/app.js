e00a5334b0a62f685e254f7fefa053e224a36d3b
Cara Giulioni <caragiulioni@gmail.com>
2021-05-13 11:31:18 -0400

    add search by keyword

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const errorMiddleware = require("./middlewares/errors");
 app.use(express.json());
 //import all routes
 
+//import all products
 const products = require("./routes/product");
 
 app.use("/api/v1", products);
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,8 @@ const Product = require("../models/product");
 
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const APIFeatures = require("../utils/apiFeatures");
+
 //Create new product => /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -13,7 +15,10 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 //get all products => api/v1/products
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find();
+  const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+
+  const products = await apiFeatures.query;
+
   res.status(200).json({ success: true, count: products.length, products });
 });
 
