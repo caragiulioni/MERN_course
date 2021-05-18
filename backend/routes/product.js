64342134347f6992364c232bf67c559c85241731
Cara Giulioni <caragiulioni@gmail.com>
2021-05-18 15:11:26 -0400

    add BE logout

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -12,9 +12,12 @@ const {
 const { isAuthenticatedUser } = require("../middlewares/auth");
 
 router.route("/products").get(isAuthenticatedUser, getProducts);
-router.route("/admin/product/new").post(newProduct);
+router.route("/admin/product/new").post(isAuthenticatedUser, newProduct);
 
-router.route("/product/:id").get(getSingleProduct);
-router.route("/admin/product/:id").put(getSingleProduct).delete(deleteProduct);
+router.route("/product/:id").get(isAuthenticatedUser, getSingleProduct);
+router
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, getSingleProduct)
+  .delete(isAuthenticatedUser, deleteProduct);
 
 module.exports = router;
