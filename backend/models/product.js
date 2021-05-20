7a89924a4c0d00a24ccd4924b92112e04e7deefc
Cara Giulioni <caragiulioni@gmail.com>
2021-05-20 08:12:52 -0400

    add forgot password token

--- backend/models/product.js
+++ backend/models/product.js
@@ -86,6 +86,10 @@ const productSchema = new mongoose.Schema({
       },
     },
   ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    reference: "User",
+  },
   createdAt: {
     type: Date,
     default: Date.now(),
